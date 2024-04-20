e357ea723ab705b7ed501afe257300454f2d2cd5
obisopulu <obisopulu@gmail.com>
2024-04-20 05:36:22 +0200

    Update layout.tsx to use Jobsavvy as the title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Techsavvy",
+  title: "Jobsavvy",
   description: "Your tech job search companion",
 };
 
