89cdf6ab4284384d51848ef42a5b18b49afda7d6
obisopulu <obisopulu@gmail.com>
2024-04-20 16:47:22 +0200

    Add Header component

---
+++ src/components/display/Header.tsx
@@ -0,0 +1,15 @@
+export default function Header() {
+
+  return (
+    <header className="bg-white">
+      <nav className=" max-w-[1024px] mx-auto flex items-center justify-between p-2 sm:px-8 font-black">
+        <div className="text-7xl">
+          &#9822;
+        </div>
+        <div className="text-5xl px-6 sm:px-3">
+          &#8285;
+        </div>
+      </nav>
+    </header>
+  )
+}
