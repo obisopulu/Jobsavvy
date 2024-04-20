1a517aa369530758bdbf6c53fa319792b718b90c
obisopulu <obisopulu@gmail.com>
2024-04-20 05:37:01 +0200

    Add ImageProps, KeywordProps, StackedListProps, and OffcanvasProps to common.type.tsx

--- src/types/common.type.tsx
+++ src/types/common.type.tsx
@@ -10,3 +10,25 @@ export type ButtonProps = {
 export type HeroProps = {
   children: React.ReactNode;
 }
+
+export type ImageProps = {
+  src: string;
+  alt: string;
+  scale: number;
+};
+
+export type KeywordProps = {
+  keyword: string;
+  found: boolean;
+};
+
+export type StackedListProps = {
+  buttons: ButtonProps[];
+  jobs: any[];
+};
+
+export type OffcanvasProps = {
+  buttons: ButtonProps[];
+  isOpen: boolean;
+  onClose: () => void;
+}
