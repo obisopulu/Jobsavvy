99fdadcefa48cdfdddc0c0d6dc527b274653ce56
obisopulu <obisopulu@gmail.com>
2024-04-21 21:34:22 +0200

    Add useToggle hook and Offcanvas component to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,25 @@ import StackedList from '@/components/display/StackedList'
 import Hero from '@/components/display/Hero'
 import TabList from '@/components/display/TabList';
 
+import { useToggle } from '@/hooks/useToggle';
+
 import { homeStackedListButtons, homeTabListButtons }from '../constants/defaults'
+import Offcanvas from '@/components/display/Offcanvas';
+import Header from '@/components/display/Header';
+
+import { jobs } from '@/constants/defaults';
 
 export default function Home() {
+  const [isActive, toggleActive] = useToggle(false);
+
   return (
     <>
+      <Header />
       <Hero>
         <TabList buttons={homeTabListButtons}/>  
       </Hero>
-      <StackedList buttons={homeStackedListButtons} /> 
+      <StackedList jobs={jobs} buttons={homeStackedListButtons} />
+      <Offcanvas isOpen={isActive} onClose={() => toggleActive()} buttons={homeStackedListButtons} /> 
     </>
   )
 }
