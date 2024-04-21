b834cb238cb806b276ae8f3313cf051f9c55c2e7
obisopulu <obisopulu@gmail.com>
2024-04-21 21:34:28 +0200

    Update Imager component to include alt attribute

--- src/components/display/Imager.tsx
+++ src/components/display/Imager.tsx
@@ -3,10 +3,10 @@
 //import Image from 'next/image';
 import { ImageProps } from '@/types/common.type';
 
-export default function Imager({src, scale}: ImageProps) {
+export default function Imager({src, alt, scale}: ImageProps) {
   return (
     <>
-      <img className={`h-${scale} w-${scale} flex-none rounded-full bg-gray-50`} src={src} alt="" />
+      <img width={scale * 4} className="flex-none rounded-lg bg-gray-50" src={src} alt={alt} />
       {/* <Image src={src} width={100 * scale} height={100 * scale} alt="Next.js Image" /> */}
     </>
   )
