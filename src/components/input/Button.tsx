ca9cabc290f0f8304f7ecf7fbc72029bed212658
obisopulu <obisopulu@gmail.com>
2024-04-21 21:35:17 +0200

    Refactor Button component to use useToggle hook and add Offcanvas functionality

--- src/components/input/Button.tsx
+++ src/components/input/Button.tsx
@@ -1,20 +1,24 @@
 'use client';
 
-import { useHandleButtonActive } from '@/hooks/useHandleButtonActive';
+import { useToggle } from '@/hooks/useToggle';
 
 import { ButtonProps } from '../../types/common.type';
+import { useState } from 'react';
 
 export default function Button({text, onClick, symbol}: ButtonProps) {
-  const [isActive, toggleActive] = useHandleButtonActive(false);
+  const [isActive, toggleActive] = useToggle(false);
+  const [openOffcanvas, setOpenOffcanvas] = useState<boolean>(false);
+
+  console.log('isActive', isActive);
   return (
       <div onClick={
         () => {
-          onClick()
-          //toggleActive()
+          toggleActive()
+          setOpenOffcanvas(true)
         }
       } className={`inline-flex gap-2 justify-center rounded-lg text-sm font-semibold py-2.5 px-4 cursor-pointer hover:bg-slate-400 hover:text-slate-900
       ${
-        isActive ? 'bg-slate-200 text-black' : 'bg-slate-900 text-white'
+        openOffcanvas ? 'bg-slate-200 text-black' : 'bg-slate-900 text-white'
       }`}>
       {symbol === 'save' && <span>&#10084;</span>}
       {symbol === 'apply' && <span>&#10148;</span>}
