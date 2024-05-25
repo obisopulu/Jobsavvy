d13cde033fda37e75b107fd2adde01fd6e598b3d
obisopulu <obisopulu@gmail.com>
2024-05-25 02:19:15 +0200

    Update TextInput component styling to include rounded corners and shadow

--- src/components/input/TextInput.tsx
+++ src/components/input/TextInput.tsx
@@ -13,7 +13,7 @@ export default function TextInput({ text, id, name, type, placeholder }: TextInp
         name={name}
         id={id}
         onChange={(e) => setValue(e.target.value)}
-        className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-black placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+        className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-black placeholder:text-gray-400 shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:text-sm sm:leading-6 rounded-md"
         value={value}
         placeholder={placeholder}
       />
