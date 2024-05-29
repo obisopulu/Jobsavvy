7bed426ef728b8e8f63557d72f9e96cae5a973da
obisopulu <obisopulu@gmail.com>
2024-05-29 14:39:57 +0200

    Update Settings component to include user settings and loading state

--- src/components/display/Settings.tsx
+++ src/components/display/Settings.tsx
@@ -1,83 +1,110 @@
 'use client';
 
-import React, { ReactNode } from 'react';
+import React, { useEffect, useState } from 'react';
+
+import { collection, getDoc, getDocs, addDoc, deleteDoc, updateDoc, doc, query, where, onSnapshot } from 'firebase/firestore';
+import { auth, db, googleProvider, gitProvider } from '@/config/firebase';
+import { createUserWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
 
 import { jobs } from '@/constants/defaults';
 
-import { OffcanvasProps } from '@/types/common.type';
+import { SettingsProps } from '@/types/common.type';
 
 import Imager from './Imager';
 import Button from '../input/Button';
 import TextInput from '../input/TextInput';
 import TextArea from '../input/TextArea';
+import Spinner from './Spinner';
 
-export default function Offcanvas({ isOpen, onClose, buttons }: OffcanvasProps){
-  const jobIndex = 2;
+export default function Settings({user}: SettingsProps){
+  
+  const [bodyOption, setOptionBody] = useState<string>('');
+  const [emailOption, setOptionEmail] = useState<string>('');
+  const [keywordsExcludeOption, setOptionKeywordsExclude] = useState<string>('');
+  const [keywordsIncludeOption, setOptionKeywordsInclude] = useState<string>('');
+  const [nameOption, setOptionName] = useState<string>('');
+  const [subjectOption, setOptionSubject] = useState<string>('');
+  const [userIdOption, setOptionUserId] = useState<string>('');
+  
+  const [loading, setLoading] = useState(true);
+  const [userOption, setUserOption] = useState<object>({
+    body: "body",
+    dateCreated: 'dateCreated',
+    keywordsExclude: "keywordsExclude",
+    keywordsInclude: "keywordsInclude",
+    name: "name",
+    subject: "subject",
+    userId: "userId"
+  });
+
+  useEffect(() => {
+    getUserSettings();
+  }, []);
+  
+  const getUserSettings = async () => {
+    setLoading(true);
+  
+    const collectionRef = collection(db, 'userSettings');
+    const q = query(collectionRef, where('userId', '==', user.uid));
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      setUserOption(doc.data());
+    });
+  
+    setLoading(false);
+  }
+
+  if (loading) {
+    return <Spinner />;
+  }
+
 
   return (
-    <div className={`
-      ${isOpen ? 'left-0' : ' left-[100%]'} z-10 top-0 fixed w-full h-full flex justify-end sm:justify-center items-center transition-all duration-500`}>
-      <div className="fixed z-20 w-full h-full backdrop-blur-sm bg-black/10 cursor-pointer" onClick={onClose}></div>
-
-      <div className="fixed z-20 w-4/5 h-full sm:h-auto sm:w-4/5 sm:max-w-[600px] bg-white sm:rounded-xl p-8  overflow-auto">
-        <div className="modal-close" onClick={onClose}>
-          <div className="text-2xl text-right font-black cursor-pointer">
-            &#10006;
-          </div>
-        </div>
-        <div className="flex justify-center">
-          <Imager src={jobs[jobIndex].firmImageUrl} scale={14} alt={jobs[jobIndex].companyName + ' - ' + jobs[jobIndex].position} />
-        </div>
-        <div className='text-center my-4'>
-          <div className='text-sm'>
-            {jobs[jobIndex].companyName}
-          </div>
-          <div className='text-center'>
-            {jobs[jobIndex].position}
-          </div>
-          <div className='text-xs text-center'>
-            {jobs[jobIndex].datePosted}
-          </div>
-        </div>
-        <div className='text-justify'>
-          {jobs[jobIndex].details}
-        </div>
-        <div className='flex flex-col text-xs text-gray-500 my-8'>
-          <div className="flex w-[250px] mx-auto justify-center">
-            <Button key={buttons[1].text} onClick={buttons[1].onClick} symbol={buttons[1].symbol} text={buttons[1].text} />
-          </div>
-          
-          <form>
-            <div className="mt-5">
-              <div>
-                <h2 className="text-base font-semibold leading-7 text-gray-900 max-w-[450px] mx-auto">Application Email</h2>
-
-                <div className="mt-2 flex flex-col gap-2">
-                  <div>
-                    <div className="mt-2">
-                      <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md w-full mx-auto">
-                        <TextInput text="Job Application for {position} at {companyName}" id={'subject'} name={'subject'} type={'text'} placeholder={'subject'} required={true} />
-                      </div>
-                    </div>
-                  </div>
-                  <div className="sm:col-span-4">
-                    <div className="mt-2">
-                      <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md w-full mx-auto">
-                        <TextArea id={'body'} name={'body'} placeholder={'body'} required={true} text="Dear Hiring Manager, \n\nI am writing to apply for the position of [position] at [companyName]. I am excited about the opportunity to contribute my skills and experience to your team.\nThank you for considering my application. I have attached my resume for your review.\n\nBest regards,\n[Your Name]" />
-                      </div>
-                    </div>
-                  </div>
-                </div>
-
-                <div className="flex w-[250px] mx-auto justify-center mt-4">
-                  <Button key={buttons[0].text} onClick={buttons[0].onClick} symbol={buttons[0].symbol} text={buttons[0].text} />
-                </div>
-              </div>
+    <div className="p-8 pt-0 lg:px-24 lg:pt-2 max-w-[1024px] mx-auto text-center">
+      {
+        loading ?
+          <Spinner />
+        :
+          <form className='w-full'>
+            <div className='flex flex-col gap-4 m-4 text-left'>
+              <div>Keywords</div>
+              <TextInput 
+                text={userOption?.keywordsInclude} 
+                id={'keywordsInclude'} 
+                name={'keywordsInclude'} 
+                type={'text'} 
+                placeholder={'keywords to include'} 
+                required={true} 
+              />
+              <TextInput 
+                text={userOption?.keywordsExclude} 
+                id={'keywordsExclude'} 
+                name={'keywordsExclude'} 
+                type={'text'} 
+                placeholder={'keywords to exclude'} 
+                required={false} 
+              />
             </div>
-
+            <div className='flex flex-col gap-4 m-4 text-left'>
+              <div>Email Template</div>
+              <TextInput 
+                text={userOption?.subject} 
+                id={'subject'} 
+                name={'subject'} 
+                type={'text'} 
+                placeholder={'subject'} 
+                required={true} 
+              />
+              <TextArea 
+                id={'body'} 
+                name={'body'} 
+                placeholder={'body'} 
+                required={true} 
+                text={userOption?.body} 
+              />
+            </div>
           </form>
-        </div>
-      </div>
+      }
     </div>
   );
 };
