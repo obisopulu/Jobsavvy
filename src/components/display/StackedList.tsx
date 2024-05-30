422dcc2c49ac76f470a710d59fde1212b3bfb5e8
obisopulu <obisopulu@gmail.com>
2024-05-31 01:38:15 +0200

    Update StackedList component to remove unnecessary details prop in JobCard

--- src/components/display/StackedList.tsx
+++ src/components/display/StackedList.tsx
@@ -10,7 +10,7 @@ export default function StackedList({ jobs, action }: StackedListProps) {
     <div className="p-8 pt-0 lg:px-24 lg:pt-2 max-w-[1024px] mx-auto">
       <ul role="list">
         {jobs.map((job, index) => (
-          <JobCard key={index} action={action} id={job.id} companyName={job.companyName} firmImageUrl={job.firmImageUrl} position={job.position} datePosted={job.datePosted} details={job.details} />
+          <JobCard key={index} action={action} id={job.id} companyName={job.companyName} firmImageUrl={job.firmImageUrl} position={job.position} datePosted={job.datePosted} />
         ))}
       </ul>
     </div>
