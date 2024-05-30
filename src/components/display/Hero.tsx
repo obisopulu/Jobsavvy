c7658651e1716e9f48b97e69b209ab2e8e43aaf7
obisopulu <obisopulu@gmail.com>
2024-05-30 03:28:07 +0200

    Update Hero component layout for better responsiveness

--- src/components/display/Hero.tsx
+++ src/components/display/Hero.tsx
@@ -4,7 +4,7 @@ import { HeroProps } from '@/types/common.type';
 
 export default function Hero({children}: HeroProps) {
   return (
-    <div className="p-8 pt-0 lg:px-24 lg:pt-32 lg:p-2 max-w-[1024px] mx-auto text-center">
+    <div className="p-8 pt-32 lg:px-24 max-w-[1024px] mx-auto text-center">
         <h2 className="text-4xl font-bold sm:text-6xl">JOBSAVVY</h2>
         {children}
         <p className="hidden mt-6 text-base bg-slate-950 rounded-lg p-8 text-white">
