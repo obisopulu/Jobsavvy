93dabbf43a059f4de4a335ed2dcadd68ba38b113
obisopulu <obisopulu@gmail.com>
2024-05-30 03:32:11 +0200

    Update TabList component to include selected button symbol

--- src/components/display/TabList.tsx
+++ src/components/display/TabList.tsx
@@ -1,14 +1,15 @@
 'use client';
 
-import { ButtonProps } from '../../types/common.type';
+import { TablistProps } from '../../types/common.type';
 import Button from "../input/Button";
 
-export default function TabList({buttons}: {buttons: ButtonProps[]}) {
+export default function TabList({buttons, selected}: TablistProps) {
+  console.log(selected.toLowerCase())
   return (
     <div className="grid grid-flow-col justify-stretch gap-6 rounded-md shadow-sm bg-slate-100 p-2">
       {
         Array.isArray(buttons) && buttons.map((button) => (
-        <Button key={button.text} onClick={button.onClick} symbol={button.symbol} text={button.text} />
+        <Button key={button.text} onClick={button.onClick} symbol={button.symbol === selected ? selected : ''} text={button.text} />
       ))}
     </div>
 
