4f6bdfc04999de02ec920df5692e681be6c1fda3
Emmanuel Tugbeh, MNCS <41299045+2gbeh@users.noreply.github.com>
2024-07-22 23:19:25 +0100

    Delete src/app/(api)/api/index.ts

--- src/app/(api)/api/index.ts
+++
@@ -1,7 +0,0 @@
-// http://127.0.0.1:3000/api
-export async function GET() {
-  return Response.json({
-    data: "Ping!",
-    meta: "https://nextjs.org/docs/app/api-reference/file-conventions/route",
-  });
-}
