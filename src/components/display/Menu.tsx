56d385f35e782d75ad336d594e0267e5263e36a6
obisopulu <obisopulu@gmail.com>
2024-07-27 15:00:37 +0200

    Refactor component imports and update menu component

--- src/components/display/Menu.tsx
+++ src/components/display/Menu.tsx
@@ -5,24 +5,24 @@ import { LogoutProps } from '../../types/common.type';
 import Imager from "./Imager";
 import Link from "next/link";
 
-export default function SignIn({logOut, user} : LogoutProps) {
+export default function Menu({logOut, user, action} : LogoutProps) {
 
   return (
-      <div className="flex items-center justify-center my-4 w-auto gap-4">
-        {
-          !window.location.href.includes('options') ?
-            <Link href={'/options'}>
-              <Button onClick={()=>{}} text={'options'} />
-            </Link>
-          :
-            <Link href={'/'}>
-              <Button onClick={()=>{}} text={'Home'} />
-            </Link>
-        }
-        <div className="bg-slate-200 rounded-3xl p-2">
-          <Imager src={user.photoURL} alt={''} scale={8} />
-        </div>
-        <Button key={'logOut'} onClick={logOut} text={'log out'} />
+    <div className="flex items-center justify-center my-4 w-auto gap-4">
+      {
+        !window.location.href.includes('options') ?
+          <Link href={'/options'}>
+            <Button onClick={()=>{}} text={'options'} />
+          </Link>
+        :
+          <Link href={'/'}>
+            <Button onClick={()=>{}} text={'Home'} />
+          </Link>
+      }
+      <div className="bg-slate-200 rounded-3xl p-2" onClick={() => action(user.photoURL)}>
+        <Imager src={user.photoURL} alt={''} scale={8} />
       </div>
+      <Button key={'logOut'} onClick={logOut} text={'log out'} />
+    </div>
   )
 }
