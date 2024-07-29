a918b417530566067dd0774ebd1ca507c495ee0d
obisopulu <obisopulu@gmail.com>
2024-07-29 12:34:37 +0200

    Update TabList component styles for improved readability

--- src/components/display/TabList.tsx
+++ src/components/display/TabList.tsx
@@ -6,7 +6,7 @@ import Button from "../input/Button";
 export default function TabList({buttons, selected}: TablistProps) {
   
   return (
-    <div className="grid grid-flow-col justify-stretch gap-6 rounded-md shadow-sm bg-slate-100 p-2">
+    <div className="grid grid-flow-col justify-stretch gap-6 rounded-md shadow-sm bg-slate-100 p-2 font-black overflow-y-scroll">
       {
         Array.isArray(buttons) && buttons.map((button) => (
         <Button key={button.text} onClick={button.onClick} symbol={button.symbol === selected ? selected : ''} text={button.text} />
