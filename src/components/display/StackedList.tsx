0fc1257e77efa3f9d0ed17577b88728bb5b55faa
obisopulu <obisopulu@gmail.com>
2024-07-31 13:22:59 +0200

    Add manage option in dashboard

--- src/components/display/StackedList.tsx
+++ src/components/display/StackedList.tsx
@@ -5,12 +5,15 @@ import { StackedListProps } from '../../types/common.type';
 
 import JobCard from './JobCard';
 
-export default function StackedList({ jobs, action }: StackedListProps) {
+export default function StackedList({ jobs, action, option }: StackedListProps) {
+
+  let complete = option === 'manage' ? 'yes' : 'no';
+
   return (
     <div className="p-8 pt-0 lg:px-24 lg:pt-2 max-w-[1024px] mx-auto">
       <ul role="list">
         {jobs.map((job, index) => (
-          <JobCard key={index} action={action} id={job.id} companyName={job.companyName} firmImageUrl={job.firmImageUrl} position={job.position} datePosted={job.datePosted} />
+          <JobCard key={index} action={action} id={job.id} companyName={job.companyName} firmImageUrl={job.firmImageUrl} position={job.position} datePosted={job.datePosted} complete={option && complete} />
         ))}
       </ul>
     </div>
