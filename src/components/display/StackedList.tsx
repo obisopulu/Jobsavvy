92f4367400cb48906386d20b4a0aa03403de736c
obisopulu <obisopulu@gmail.com>
2024-08-01 00:29:25 +0200

    Refactor StackedList component to improve code readability and performance

--- src/components/display/StackedList.tsx
+++ src/components/display/StackedList.tsx
@@ -6,15 +6,31 @@ import { StackedListProps } from '../../types/common.type';
 import JobCard from './JobCard';
 
 export default function StackedList({ jobs, action, option }: StackedListProps) {
-
-  let complete = option === 'manage' ? 'yes' : 'no';
-
   return (
     <div className="p-8 pt-0 lg:px-24 lg:pt-2 max-w-[1024px] mx-auto">
       <ul role="list">
-        {jobs.map((job, index) => (
-          <JobCard key={index} action={action} id={job.id} companyName={job.companyName} firmImageUrl={job.firmImageUrl} position={job.position} datePosted={job.datePosted} complete={option && complete} />
-        ))}
+        {jobs.map((job, index) => {
+          let complete = false
+          if(job.jobCompany && job.jobDetails && job.jobEmail && job.jobImage && job.jobPosition)
+            complete =  true;
+
+            console.log(complete, job)
+
+          return (
+            (option == 'manage' || (option != 'manage' && complete == true)) && (
+              <JobCard 
+                key={index} 
+                action={action} 
+                id={job.id || '1'} 
+                jobCompany={job.jobCompany} 
+                jobImage={job.jobImage} 
+                jobPosition={job.jobPosition} 
+                dateCreated={job.dateCreated} 
+                complete={complete} 
+              />
+            )
+          );
+        })}
       </ul>
     </div>
   )
